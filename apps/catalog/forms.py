cfb88f005edcf4fd11be4893f38d76a4efef2abe
Lonewolfrex <samparkprakash27@gmail.com>
2026-02-01 10:15:07 +0530

    Database and more features configured

--- apps/catalog/forms.py
+++ apps/catalog/forms.py
@@ -2,15 +2,20 @@ from django import forms
 from .models import Book
 
 class BuyBookForm(forms.Form):
-    book_id = forms.IntegerField(widget=forms.HiddenInput)
+    book_id = forms.IntegerField(widget=forms.HiddenInput())
     quantity = forms.IntegerField(
-        min_value=1,
+        min_value=1, 
+        max_value=10,  # Max 10 per purchase
         initial=1,
-        widget=forms.NumberInput(attrs={'class': 'form-control', 'min': '1'})
+        widget=forms.NumberInput(attrs={
+            'class': 'form-control', 
+            'min': '1', 
+            'max': '10'
+        })
     )
 
 class RentBookForm(forms.Form):
-    book_id = forms.IntegerField(widget=forms.HiddenInput)
+    book_id = forms.IntegerField(widget=forms.HiddenInput())
     rent_days = forms.ChoiceField(
         choices=[(7, '1 Week'), (14, '2 Weeks'), (30, '1 Month')],
         widget=forms.Select(attrs={'class': 'form-select'})
